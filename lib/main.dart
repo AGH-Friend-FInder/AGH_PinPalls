9af20921cf2049b3cf258db7aae89e1f5c478a2d
Kacper Kozubowski <agapanthus133@gmail.com>
2025-04-12 09:56:50 +0200

    Add basic screen structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,76 +1,31 @@
+import 'package:agh_pin_palls/screens/groups_screen.dart';
+import 'package:agh_pin_palls/screens/settings_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:agh_pin_palls/map.dart';
+import 'screens/login_screen.dart';
+import 'screens/map_screen.dart';
 
 void main() {
-  runApp(const App());
+  runApp(const PinPalsApp());
 }
 
-class App extends StatelessWidget {
-  const App({super.key});
+class PinPalsApp extends StatelessWidget {
+  const PinPalsApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'AGH PinPals',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        primarySwatch: Colors.green,
       ),
-      home: const HomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: Builder(
-          builder: (context) {
-            return IconButton(
-              icon: const Icon(Icons.menu),
-              onPressed: () {
-                Scaffold.of(context).openDrawer();
-              },
-            );
-          },
-        ),
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(title),
-      ),
-      body: Center(child: MapScreen()),
-      drawer: Drawer(
-        child: ListView(
-          // Important: Remove any padding from the ListView.
-          padding: EdgeInsets.zero,
-          children: [
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: Theme.of(context).colorScheme.inversePrimary,
-              ),
-              child: Text('Drawer Header'),
-            ),
-            ListTile(
-              title: const Text('Item 1'),
-              onTap: () {
-                // Update the state of the app.
-                // ...
-              },
-            ),
-            ListTile(
-              title: const Text('Item 2'),
-              onTap: () {
-                // Update the state of the app.
-                // ...
-              },
-            ),
-          ],
-        ),
-      ),
+      // Zawsze startuje od ekranu logowania
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => const LoginScreen(),
+        '/map': (context) => const MapScreen(),
+        '/settings': (context) => const SettingsScreen(),
+        '/groups': (context) => const GroupsScreen()
+      },
     );
   }
 }
