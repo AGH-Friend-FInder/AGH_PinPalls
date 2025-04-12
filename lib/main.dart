9af3f123c2f555176758c43f288ce50a6c24e5a9
joannakonieczny <konieczny.joasia@gmail.com>
2025-04-12 11:34:04 +0200

    register screen + login button

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:agh_pin_palls/screens/settings_screen.dart';
 import 'package:flutter/material.dart';
 import 'screens/login_screen.dart';
 import 'screens/map_screen.dart';
+import 'screens/register_screen.dart';
 
 void main() {
   runApp(const PinPalsApp());
@@ -16,13 +17,13 @@ class PinPalsApp extends StatelessWidget {
     return MaterialApp(
       title: 'AGH PinPals',
       theme: ThemeData(primarySwatch: Colors.green),
-      // Zawsze startuje od ekranu logowania
       initialRoute: '/login',
       routes: {
         '/login': (context) => LoginScreen(),
         '/map': (context) => const MapScreen(),
         '/settings': (context) => const SettingsScreen(),
         '/groups': (context) => const GroupsScreen(),
+        '/register': (context) => RegisterScreen(),
       },
     );
   }
